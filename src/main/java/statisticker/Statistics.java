168fc043ed262f436e69ee811b74c95ac4b275da
Philips-Varun-Kashyap <varun.kashyap@philips.com>
2026-05-21 21:33:04 +0530

    test driven implementation first draft

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+    public static Stats getStatistics(List<Float> numbers) {
+        if (numbers.isEmpty()) {
+            return new Stats(Float.NaN, Float.NaN, Float.NaN);
+        }
+
+        float sum = 0.0f;
+        float min = numbers.get(0);
+        float max = numbers.get(0);
+
+        for (float number : numbers) {
+            sum += number;
+            min = Math.min(min, number);
+            max = Math.max(max, number);
+        }
+
+        return new Stats(sum / numbers.size(), min, max);
     }
 }
---
+++ src/main/java/statisticker/Stats.java
@@ -0,0 +1,15 @@
+package statisticker;
+
+public class Stats
+{
+    public final float average;
+    public final float min;
+    public final float max;
+
+    public Stats(float average, float min, float max)
+    {
+        this.average = average;
+        this.min = min;
+        this.max = max;
+    }
+}
